0fcd9fc9c39ac8049b0649b52bd999e549731faa
ateeq <atiqueraza98@gmail.com>
2024-10-21 13:25:28 +0530

    finish core sketchpad

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const container = document.querySelector(".container");
+const resetButton = document.getElementById('resetButton')
 
 for(let i = 0; i < 16 * 16; i++){
     const square = document.createElement('div');
     square.classList.add('item');
     container.appendChild(square);
+
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor ='#36322c'
+    })
 }
+
+resetButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.item');
+    squares.forEach(square => {
+        square.style.backgroundColor = '';
+    })
+})
